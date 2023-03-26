7c2b83dd34fb792df98b48c9ce4c6a339ecc2562
Andrey Kharginen <kut13@inbox.ru>
2023-03-26 14:06:27 +0400

    add test error

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -11,4 +11,20 @@ public class CashbackHackServiceTest {
         int expected = 200;
         Assert.assertEquals(actual, expected);
     }
+    @Test
+    public void remain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void remain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        Assert.assertEquals(actual, expected);
+    }
 }
