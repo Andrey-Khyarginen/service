93099e5a59b0d82b523508f18817d333944fa7dc
Andrey Kharginen <kut13@inbox.ru>
2023-03-29 21:12:03 +0400

    change test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -6,26 +6,23 @@ public class CashbackHackServiceTest {
 
     @Test
     public void remain() {
-        int amount = 800;
         int expected = 200;
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
+        int actual = service.remain(800);
         assertEquals(actual, expected);
     }
     @Test
-    public void remain2() {
-        int amount = 1000;
+    public void test() {
         int expected = 0;
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
+        int actual = service.remain(1000);
         assertEquals(actual, expected);
     }
     @Test
-    public void remain3() {
-        int amount = 1700;
+    public void test3() {
         int expected = 300;
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
+        int actual = service.remain(1700);
         assertEquals(actual, expected);
     }
 }
